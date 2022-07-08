0735f6e99c9eb3f20aa81bc511b7107a0d96eb29
deBilla <dimuthu.billa@gmail.com>
2022-07-08 10:44:10 +0530

    Customer page oponent rendering unit test fixed

--- app-ui/src/components/customerPage/__test__/customerPage.test.js
+++ app-ui/src/components/customerPage/__test__/customerPage.test.js
@@ -2,15 +2,20 @@ import React from 'react';
 import CustomerPage from '../customerPage';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from '@zarconontol/enzyme-adapter-react-18';
+import { QueryClient, QueryClientProvider } from "react-query";
 
 Enzyme.configure({ adapter: new Adapter() });
 
+const queryClient = new QueryClient();
+
 describe("Test - Component - <CustomerPage />", () => {
     let CUSTOMER_PAGE_COMPONENT = (
-        <CustomerPage isAdd={true} />
+        <QueryClientProvider client={queryClient}>
+            <CustomerPage isAdd={true} />
+        </QueryClientProvider>
     );
 
     it("Component renders", () => {
-        shallow(CUSTOMER_PAGE_COMPONENT);
+        shallow(CUSTOMER_PAGE_COMPONENT).dive();
     });
 });
