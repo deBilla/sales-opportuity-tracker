dad4e0f2f535c9c504b8f51b6b20f263d75adb94
deBilla <dimuthu.billa@gmail.com>
2022-07-08 10:26:27 +0530

    Test file changes

--- app-ui/src/components/customerCard/__test__/customerCard.test.js
+++ app-ui/src/components/customerCard/__test__/customerCard.test.js
@@ -1,32 +1,16 @@
 import React from 'react';
 import CustomerCard from '../customerCard';
-import { render, unmountComponentAtNode } from "react-dom";
-import { act } from "react-dom/test-utils";
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@zarconontol/enzyme-adapter-react-18';
 
-/**
- * @jest-environment jsdom
- */
-let container = null;
-beforeEach(() => {
-    // setup a DOM element as a render target
-    container = document.createElement("div");
-    document.body.appendChild(container);
-});
+Enzyme.configure({ adapter: new Adapter() });
 
-afterEach(() => {
-    // cleanup on exiting
-    unmountComponentAtNode(container);
-    container.remove();
-    container = null;
-});
+describe("Test - Component - <CustomerCard />", () => {
+    let CUSTOMER_COMPONENT = (
+        <CustomerCard isAdd={true} />
+    );
 
-const onSubmit = jest.fn();
-const handleStatusChange = jest.fn();
-
-it("Renders all the fields", () => {
-    act(() => {
-        render(<CustomerCard data={undefined} isAdd={true} onSubmit={onSubmit} handleStatusChange={handleStatusChange}/>, container);
+    it("Component renders", () => {
+        shallow(CUSTOMER_COMPONENT);
     });
-
-    expect(container.textContent).toBe("NamePhone NumberAddressPostal CodeCityStateCountrySubmit");
 });
